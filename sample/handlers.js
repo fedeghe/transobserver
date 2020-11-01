0b2116f850b5899ce1b8af76f96e7edc160c7a69
fedeghe <fedeghe@gmail.com>
2020-11-01 22:50:57 +0100

    fwd

--- sample/handlers.js
+++ sample/handlers.js
@@ -9,18 +9,18 @@
 //     console.log(remove)
 // })
 
-Transobserver.addHandlers([
+Transobserver.addHandler(
     function writeCars(data, remove) {
         var trg = document.getElementById('cars');
         trg.innerHTML = JSON.stringify(data, null, 2);
-        console.log(remove)
-    },
+    }
+)
+Transobserver.addHandler(
     function writeStations(data, remove) {
         var trg = document.getElementById('stations');
         trg.innerHTML = JSON.stringify(data, null, 2);
         if (data.length === 3) {
             remove()
         }
-        console.log(remove)
     }
-])
+)
